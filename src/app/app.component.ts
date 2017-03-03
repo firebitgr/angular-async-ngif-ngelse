8c4719ebb15baa194e12121a85692306c2d0337e
blAck PR <blackpr.tim@gmail.com>
2017-03-03 13:16:48 +0200

    add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { TriviaService } from './trivia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  question$: Observable<any>;
+
+  constructor(private trivia:TriviaService) { }
+
+  ngOnInit() {
+    this.question$ = this.trivia.getQuestions();
+  }
 }
